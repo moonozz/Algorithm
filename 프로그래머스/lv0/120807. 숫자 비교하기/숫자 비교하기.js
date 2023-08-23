cf7267573f05185e00e1b2192cdd2225070f5a87
Moono <94160557+moonozz@users.noreply.github.com>
2023-08-23 12:59:28 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -0,0 +1,10 @@
+function solution(num1, num2) {
+    // 두 수가 같으면 1, 다르면 -1
+    
+    // let answer = 0;
+
+    if (num1 === num2) return 1; // 두 수가 같으면 1
+    return -1 // 다르면 -1
+        
+    // return answer;
+}
