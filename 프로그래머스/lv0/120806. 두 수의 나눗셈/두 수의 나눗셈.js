31074c1e26f3d4179a04485e99b44d47ddb3c757
Moono <94160557+moonozz@users.noreply.github.com>
2023-08-26 18:32:46 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.js
@@ -0,0 +1,9 @@
+function solution(num1, num2) {
+    // (num1 / num2) * 1000
+    // 소수점 날려버리기 Math.floor()
+    let answer = 0;
+    
+    // answer = Math.floor((num1 / num2) * 1000);
+    
+    return answer = Math.floor((num1 / num2) * 1000);
+}
