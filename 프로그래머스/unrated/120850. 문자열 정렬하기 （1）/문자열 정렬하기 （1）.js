894cae1d209e87847d962ec089d7d97bd0f92c81
Moono <94160557+moonozz@users.noreply.github.com>
2023-12-01 22:29:28 +0900

    [level unrated] Title: 문자열 정렬하기 (1), Time: 0.16 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/unrated/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,7 +1,13 @@
 function solution(my_string) {
     let answer = [];
     
-    return [...my_string].map((a) => Number(a)).filter((a) => !isNaN(a)).sort((a,b) => a - b);
+    return [...my_string].map(function(a) {
+        return Number(a)
+    }).filter(function(a) {
+        return !isNaN(a)
+    }).sort((a,b) => a - b);
     
     return answer;
 }
+
+// [...my_string].map((a) => Number(a)).filter((a) => !isNaN(a)).sort((a,b) => a - b);
